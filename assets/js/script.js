ca56ab0ada97cbafa4f749860db2e34fffd7f26f
forbegos <forbegos@me.com>
2021-04-25 18:18:42 -0600

    Initial declarations for testing.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,29 @@
 // CODING QUIZ PROGRAM
 //---- Declaring arrays with questions and answers (strings) so it is easier to add or replace questions in the future,
 // maybe populating from a text file------
-// ------------------------------------------
+// ---------------------------------------------------------------------------------------
 // Declare array with questions
-
+var questionList = ["q1", "q2", "q3", "q4", "q5"];
 // Declare array with answers
-//-----------------------------------------
+var answersList = ["a1", "a2", "a3", "a4", "a5"];
+//-----------------------------------------------------------------------------------------
 
+//-------------------------------------- DECLARATIONS --------------------------------------
 // Declare object question
+var question = {
+  Q: "",
+  A: "",
+};
+
+// Declare function populate (array questions, array answers) and returns array of question objects -> question Array
+function populate(Qlist, AList, question) {}
+// Declare function askQuestions (QA)
+function askQuestions(questionArray) {}
+// Declare function evaluateAnswer (QA)
+function evaluateAnswer(questionArray) {}
+
+// ----------------------- Execution ----------------------------
 
 // Declare function startQuiz
-
-// Initiate timer function
-
-// Declare function populate (array questions, array answers) and returns array of objects QA
-
-// Declare function askQuestions (QA)
-
-// Declare function evaluateAnswer (QA)
-
+function startQuiz() {}
 // startQuiz()
