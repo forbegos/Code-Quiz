8cf58d804ce315c16a46a5ae14e84a25ff896cd3
forbegos <forbegos@me.com>
2021-04-25 18:07:52 -0600

    Pseudocode v1.0

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// CODING QUIZ PROGRAM
+//---- Declaring arrays with questions and answers (strings) so it is easier to add or replace questions in the future,
+// maybe populating from a text file------
+// ------------------------------------------
+// Declare array with questions
+
+// Declare array with answers
+//-----------------------------------------
+
+// Declare object question
+
+// Declare function startQuiz
+
+// Initiate timer function
+
+// Declare function populate (array questions, array answers) and returns array of objects QA
+
+// Declare function askQuestions (QA)
+
+// Declare function evaluateAnswer (QA)
+
+// startQuiz()
