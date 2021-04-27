5133763be58e702c5a24466c19c6f4805096cb98
forbegos <forbegos@me.com>
2021-04-26 19:42:35 -0600

    tested declaring and pushing into an array of objects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,41 @@
 // CODING QUIZ PROGRAM
-//---- Declaring arrays with questions and answers (strings) so it is easier to add or replace questions in the future,
-// maybe populating from a text file------
-// ---------------------------------------------------------------------------------------
-// Declare array with questions
-var questionList = ["q1", "q2", "q3", "q4", "q5"];
-// Declare array with answers
-var answersList = ["a1", "a2", "a3", "a4", "a5"];
-//-----------------------------------------------------------------------------------------
 
-//-------------------------------------- DECLARATIONS --------------------------------------
-// Declare object question
-var question = {
-  Q: "",
-  A: "",
-};
+//--  DECLARATIONS ----
 
-// Declare function populate (array questions, array answers) and returns array of question objects -> question Array
-function populate(Qlist, AList, question) {}
+// Declare questions array and options array for each question:
+var questionList = [
+  "Commonly used data types DO NOT include:",
+  "The condition in an if/else statement is enclosed with _____.",
+  "Arrays in JavaScript can be used to store _____.",
+  "String values must be enclosed within ______ when being assigned to variables.",
+  "A very useful tool used during development and debugging for printing content to the debugger is:",
+];
+
+var optionsArray = [
+  ["strings", "booleans", "alerts", "numbers"],
+  ["quotes", "curly brackets", "parenthesis", "square brackets"],
+];
+
+console.log(optionsArray);
+
+// Declare answers array:
+var answers = ["alerts", "parenthesis"];
+// Declare new array:
+var questionArray = new Array();
+
+function populate() {
+  for (let i = 0; i < 2; i++) {
+    console.log(i);
+    questionArray.push(new Object());
+    questionArray[i].question = questionList[i];
+    questionArray[i].options = optionsArray[i];
+    questionArray[i].answer = "TEST";
+  }
+}
+
+populate();
+console.log(questionArray);
+
 // Declare function askQuestions (QA)
 function askQuestions(questionArray) {}
 // Declare function evaluateAnswer (QA)
